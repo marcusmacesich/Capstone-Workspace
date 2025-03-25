6af31d3d647cd9c1d48ab0c92c013c9c9aa65a54
KeeganFrey <96201768+KeeganFrey@users.noreply.github.com>
2025-03-25 10:08:45 -0400

    Update install_janus_ubuntu.sh

--- install_janus_ubuntu.sh
+++ install_janus_ubuntu.sh
@@ -7,13 +7,12 @@ sudo apt install -y libmicrohttpd-dev libjansson-dev \
 	libssl-dev libsofia-sip-ua-dev libglib2.0-dev \
 	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
 	libconfig-dev pkg-config zlib1g-dev libusrsctp-dev libwebsockets-dev \
-        lobnice-dev libsrtp2-dev aptitude
+        libnice-dev libsrtp2-dev aptitude
 
-#The janus documentation say to do it this way rather than apt install
-#The other way is to use sudo apt install libnice-dev
-git clone https://gitlab.freedesktop.org/libnice/libnice
-cd libnice
-meson --prefix=/usr build && ninja -C build && sudo ninja -C build install
+#The janus documentation say to do it this way rather than apt install, so if something is going wrong, uncomment out the following and remove libnice from the apt isntall (second to last)
+#git clone https://gitlab.freedesktop.org/libnice/libnice
+#cd libnice
+#meson --prefix=/usr build && ninja -C build && sudo ninja -C build install
 
 sudo aptitude install doxygen graphviz
 
