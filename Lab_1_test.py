f2aec9c252a0f70d3a26881ddfac6d3575c7b7a6
KeeganFrey <96201768+KeeganFrey@users.noreply.github.com>
2025-04-05 17:47:20 -0400

    Add files via upload

---
+++ Lab_1_test.py
@@ -0,0 +1,63 @@
+inport numpy as np
+from Arm_Lib_New inport Arm_Device1
+import time
+Arm = Arm_Device190
+time.sleep(.1)
+
+#send home 
+Arm.Arm_serial_servo_write6_array([90 90 90 90 90 90],4000)
+
+#read home angles
+jointsAng = Pos_array()
+print(jointsAng)
+#read servo home position coordinates
+jointsPos = get_pos_xyz_proj(jointsAng)
+print(jointsPos)
+#send servos 2-4 independently to 0 and 180 deg and read position coordinates
+#Send Servo 2 to 0 & 90 and read
+Arm_serial_servo_write(2, 0, 1000)
+Serv2_0 = Get_pos_xyz()
+print(Serv2_0)
+Arm_serial_servo_write(2, 90, 1000)
+Serv2_90 =Get_pos_xyz()
+print(Serv2_90)
+#Reset
+Arm.Arm_serial_servo_write6_array([90 90 90 90 90 90],4000)
+
+#Send Servo 3 to 0 & 90 and read
+Arm_serial_servo_write(3, 0, 1000)
+Serv3_0 = Get_pos_xyz()
+print(Serv3_0)
+Arm_serial_servo_write(3, 90, 1000)
+Serv3_90 =Get_pos_xyz()
+print(Serv3_90)
+#Reset
+Arm.Arm_serial_servo_write6_array([90 90 90 90 90 90],4000)
+
+#Send Servo 4 to 0 & 90 and read
+Arm_serial_servo_write(4, 0, 1000)
+Serv4_0 = Get_pos_xyz()
+print(Serv4_0)
+Arm_serial_servo_write(4, 90, 1000)
+Serv4_90 =Get_pos_xyz()
+print(Serv4_90)
+#Reset
+Arm.Arm_serial_servo_write6_array([90 90 90 90 90 90],4000)
+
+#send servo 4 to 0 and 90 deg and and read position coordinates
+Arm_serial_servo_write(4, 90, 1000)
+print(Get_pos_xyz())
+#send servo 1 to 90 and 180 deg and read position coordinates
+Arm_serial_servo_write(1, 180, 1000)
+print(Get_pos_xyz())
+
+#Part 2 - Pick and place
+# follow directions in lab manual to perform the following task
+#start in home position
+#open gripper (servo 6) to 180 deg
+#close gripper (servo 6) to XXX degrees
+#return home
+#place block in position 2
+#open grippers
+#return home
+#record final servo andles and position coordinates necessary to perform this task
